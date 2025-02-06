fec1d7caf08508db5ff04f2109404920168bc96f
SabirHalil <sabir47halil@gmail.com>
2025-02-06 12:31:25 +0300

    updated employee processes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e6056b9c02d86c77543040e975be5c17",
+const RESOURCES = {"flutter_bootstrap.js": "1bfc248b89cc660a6ee11f5b1ad06e22",
 "version.json": "8fd45493493a43c6c4f75665b64d9246",
 "splash/img/light-2x.png": "71f12ec6478438f33a2bcf0567b9fb9e",
 "splash/img/dark-4x.png": "f489e9eb072b331b49822ead42784895",
@@ -13,9 +13,9 @@ const RESOURCES = {"flutter_bootstrap.js": "e6056b9c02d86c77543040e975be5c17",
 "splash/img/dark-2x.png": "71f12ec6478438f33a2bcf0567b9fb9e",
 "splash/img/dark-1x.png": "929216e86c341394a367b7ee5180dc98",
 "splash/img/light-1x.png": "929216e86c341394a367b7ee5180dc98",
-"index.html": "c43f990aa5dfc08821b0c8617e7bfa0a",
-"/": "c43f990aa5dfc08821b0c8617e7bfa0a",
-"main.dart.js": "7c3c4683cea339f442a75d7678793a3f",
+"index.html": "f0af6bd570590314e71a05e0c93b6ca7",
+"/": "f0af6bd570590314e71a05e0c93b6ca7",
+"main.dart.js": "38a97bb83408b0466bd0b9bd21c82b73",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "569a383765aa4c7e3d66ea73c916a2b6",
 "icons/Icon-192.png": "8779fce76573fbd768a2145d65feecce",
