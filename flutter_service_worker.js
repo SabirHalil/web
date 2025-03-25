97697fb4df13f386ffe0e542241a207bb3f00f65
SabirHalil <sabir47halil@gmail.com>
2025-03-25 23:41:41 +0300

    updated ramadan odemesi texts

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "933fc0ee35a50ff6ac2cb27f50f41ccb",
+const RESOURCES = {"flutter_bootstrap.js": "4fc8d4299537be65117a7fb8dbef4093",
 "version.json": "8fd45493493a43c6c4f75665b64d9246",
 "splash/img/light-2x.png": "71f12ec6478438f33a2bcf0567b9fb9e",
 "splash/img/dark-4x.png": "f489e9eb072b331b49822ead42784895",
@@ -13,9 +13,9 @@ const RESOURCES = {"flutter_bootstrap.js": "933fc0ee35a50ff6ac2cb27f50f41ccb",
 "splash/img/dark-2x.png": "71f12ec6478438f33a2bcf0567b9fb9e",
 "splash/img/dark-1x.png": "929216e86c341394a367b7ee5180dc98",
 "splash/img/light-1x.png": "929216e86c341394a367b7ee5180dc98",
-"index.html": "ab82a6eab93079853c81c819a682a702",
-"/": "ab82a6eab93079853c81c819a682a702",
-"main.dart.js": "5a0c4d81a00163860da18ce97bb2f24f",
+"index.html": "b177f70d446267066469bfae4baed9da",
+"/": "b177f70d446267066469bfae4baed9da",
+"main.dart.js": "f104f1b960f47c9b85716bfdf72f2c21",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "569a383765aa4c7e3d66ea73c916a2b6",
 "icons/Icon-192.png": "8779fce76573fbd768a2145d65feecce",
